7412d380c5e2c4f6016d3995de5aad5dbd5fa463
Ankit Banerjee <63962369+ankitbanerjee0211@users.noreply.github.com>
2023-02-07 21:42:37 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        // rw.name = "Ankit";
+        // System.out.println(rw.name);
+
+        // Error Message:
+        // Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+        // The field RWOnly.name is not visible
+
+        // at com.driver.Main.main(Main.java:6)
+
+        rw.setName("Ankit");
+        System.out.println(rw.getName());
+    }
 }
